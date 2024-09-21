961b05b2ec4e9eef233a316d9394afdca13b296c
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-09-21 14:24:25 +0530

    Clone a linked list with next and random pointer(21 sept).js

--- September 2024/Clone a linked list with next and random pointer(21 sept).js
+++ September 2024/Clone a linked list with next and random pointer(21 sept).js
@@ -1,2 +1,32 @@
 // JAVA CODE
 
+class Solution {
+    // Function to clone a linked list with next and random pointer.
+      Node copyList(Node head) {
+        // your code here
+        Node temp = head;
+        while(temp!=null){
+            Node cloneNode = new Node(temp.data);
+            cloneNode.next=temp.next;
+            temp.next=cloneNode;
+            temp=cloneNode.next;
+        }
+        temp=head;
+        while(temp!=null){
+            if(temp.random!=null){
+                temp.next.random=temp.random.next;
+            }
+            temp=temp.next.next;
+        }
+        Node clone = new Node(0), ans = clone;
+        temp=head;
+        while(temp!=null){
+            clone.next=temp.next;
+            clone=clone.next;
+            temp.next=clone.next;
+            temp=temp.next;
+        }
+        return ans.next;
+    }
+
+}
