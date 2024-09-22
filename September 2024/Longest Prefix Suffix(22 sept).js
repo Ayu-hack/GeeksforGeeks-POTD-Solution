a8ff93d8a5dfc186c6d95c104a29b9885bdb9566
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-09-22 12:57:27 +0530

    Longest Prefix Suffix(22 sept).js

---
+++ September 2024/Longest Prefix Suffix(22 sept).js
@@ -0,0 +1,18 @@
+//JAVA CODE 
+
+class Solution {
+     int lps(String str) {
+        // code here
+        int i=1,j=0,n=str.length();
+        int storeLength[] = new int[n];
+        while(i<n){
+            if(str.charAt(i)==str.charAt(j))storeLength[i++]=++j;
+            else{
+                if(j>0)j=storeLength[j-1];
+                else i++;
+            }
+        }
+        return storeLength[n-1];
+    }
+    
+}
