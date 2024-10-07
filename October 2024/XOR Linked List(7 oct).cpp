db2db0ee2fa30850e60b9da2ab6723ccf34af73a
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-10-07 20:37:16 +0530

    XOR Linked List(7 oct).cpp

---
+++ October 2024/XOR Linked List(7 oct).cpp
@@ -0,0 +1,20 @@
+struct Node *insert(struct Node *head, int data) {
+    // Code here
+    Node *temp = new Node(data);
+    temp->npx = head;
+    if(head)head->npx = XOR(temp,head->npx);
+    return temp;
+}
+
+vector<int> getList(struct Node *head) {
+    // Code here
+    vector<int> ans;
+    Node *curr=head,*prev=NULL,*nxt=NULL;
+    while(curr){
+        ans.push_back(curr->data);
+        nxt = XOR(prev,curr->npx);
+        prev=curr;
+        curr=nxt;
+    }
+    return ans;
+}
