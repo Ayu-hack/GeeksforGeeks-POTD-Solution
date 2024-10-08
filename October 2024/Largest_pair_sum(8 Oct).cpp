d23307d180e0430e877def8c253c4700a51bab97
Basten Ahamad Molla <bastenahamad@gmail.com>
2024-10-08 23:42:57 +0530

    Revert "Issue: #11 Added July 2024 POTDs"

--- October 2024/Largest_pair_sum(8 Oct).cpp
+++ October 2024/Largest_pair_sum(8 Oct).cpp
@@ -1,16 +1,12 @@
 class Solution {
   public:
     int pairsum(vector<int> &arr) {
-        int mx1 = 0, mx2 = 0;
-        for(int i=0; i<arr.size(); i++) {
-            if(arr[i] > mx1) {
-                mx2 = mx1;
-                mx1 = arr[i];
-            }
-            else if(arr[i] > mx2) {
-                mx2 = arr[i];
-            }
-        }
-        return mx1 + mx2;
+        
+        sort(arr.begin(), arr.end());
+        
+        int n = arr.size();
+        
+        return arr[n-2] + arr[n-1];
+        // code here
     }
 };
