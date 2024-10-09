8b2b3caa94cc41911d578120f491ed4c037e0684
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-10-09 13:35:20 +0530

    Create Linked List Matrix(9 Oct).java

---
+++ October 2024/Linked List Matrix(9 Oct).java
@@ -0,0 +1,24 @@
+// JAVA CODE 
+
+class Solution {
+    static Node construct(int arr[][]) {
+        // Add your code here.
+        int n = arr.length;
+        Node upper=null, prev=null, start=null, head=null;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                Node temp = new Node(arr[i][j]);
+                if(head==null)head=temp;
+                if(upper==null)upper=start;
+                if(i!=0){
+                    upper.down=temp;
+                    upper=upper.right;
+                }
+                if(j==0)start=temp;
+                if(j!=0)prev.right=temp;
+                prev=temp;
+            }
+        }
+        return head;
+    }
+}
