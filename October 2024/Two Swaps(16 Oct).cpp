674f84f3b9a10c724f81a2c8a22d5aedc333356c
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-10-16 22:19:48 +0530

    Two Swaps(16 Oct).cpp

---
+++ October 2024/Two Swaps(16 Oct).cpp
@@ -0,0 +1,18 @@
+// C++ CODE
+
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+        // code here.
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]!=i+1){
+                swap(arr[i],arr[arr[i]-1]);
+                count++;
+                i--;
+            }
+            if(count>2)return false;
+        }
+        return count==2|count==0;
+    }
+};
