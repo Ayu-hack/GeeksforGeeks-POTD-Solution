646e32364eba970e2f995a4dc8c83e8f311293a4
mrsandy1965 <123884782+mrsandy1965@users.noreply.github.com>
2024-10-17 12:20:11 +0530

    Create Insert_an_Element_at_the_Bottom_of_a_Stack(24.py

---
+++ March 2024/Insert_an_Element_at_the_Bottom_of_a_Stack.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+class Solution:
+    def insertAtBottom(self,st,x):
+        # code here
+        st.insert(0,x)
+        return st
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n,x = map(int,input().split())
+        stack = list(map(int,input().split()))
+        obj = Solution()
+        ans = obj.insertAtBottom(stack,x)
+        for e in ans:
+            print(e,end=" ")
+        print()
+        
+        
+        
+# } Driver Code Ends
