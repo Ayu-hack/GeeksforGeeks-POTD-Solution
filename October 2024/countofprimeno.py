4f9fe0b356169b907001bd042ce65fd13e04d52d
Birajit Saikia <birajitsaikia@gmail.com>
2024-10-18 10:36:57 +0530

    Remove unwanted files from PR

--- October 2024/countofprimeno.py
+++
@@ -1,8 +0,0 @@
-def check_total_primes(N,M):
-    prime_count = 0
-    for num in range(N,M+1):
-        if check_prime(num) == 1:
-            prime_count += 1
-    return prime_count
-N,M = map(int, input().split())
-print(check_total_primes(N,M))
--- October 2024/rightangletriangle.py
+++
@@ -1,6 +0,0 @@
-def patternPrinting(n):
-    for i in range(1, n + 1):
-        for j in range(1, i + 1):
-            print(j, end=" ")
-        print()
-patternPrinting(n)
