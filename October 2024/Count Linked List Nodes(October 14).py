da594f243c108ca4f08bcf2f4acf037010a4b144
Abhinav S <87542795+AbhiKamp@users.noreply.github.com>
2024-10-23 09:47:50 +0530

    Find the Sum of Last N nodes of the Linked List
    Find the Sum of Last N nodes of the Linked List using python. Question for October 23.

--- October 2024/Count Linked List Nodes(October 14).py
+++ October 2024/Count Linked List Nodes(October 14).py
@@ -1,25 +1,9 @@
-#User function Template for python3
 
-'''
-
-#Linked list class
-class LinkedList:
-    def __init__(self):
-        self.head=None
-        self.tail=None
-        '''
 class Solution:
     # Function to count nodes of a linked list.
     def getCount(self, head):
         a = max(arr)
         return a
-
-        # code here
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
 
 
 class Node:
