7e6c9b705670e3ca170b5e0eefdc1d1e03a893f7
SWM32 <98332411+SWM32@users.noreply.github.com>
2024-10-23 17:29:12 +0530

    Solved Oct 19 Problem using Python

---
+++ October 2024/Nearest Multiple of 10 (Oct 19).py
@@ -0,0 +1,29 @@
+#User function Template for python3
+import sys
+sys.set_int_max_str_digits(0)
+
+class Solution:
+    def roundToNearest (self, str1) : 
+        #Complete the function
+        l = len(str1)
+        s = int(str1)
+        if s%10 <= 5:
+            s -= (s%10)
+            s =  str(s).rjust(l,'0')
+            return s
+        else:
+            s = (s - (s%10)) + 10
+            s =  str(s).rjust(l,'0')
+            return s
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+for _ in range(0, int(input())):
+    num_str = input()
+    ob = Solution()
+    res = ob.roundToNearest(num_str)
+    print(res)
+
+# } Driver Code Ends
