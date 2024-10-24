cf8f53c8a49cae9a85b5c0bd69b10c4713ba254a
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-10-24 23:25:00 +0530

    Create Modify the Array(24 oct).java

---
+++ October 2024/Modify the Array(24 oct).java
@@ -0,0 +1,26 @@
+class Solution {
+   static ArrayList<Integer> modifyAndRearrangeArr(int arr[]) {
+        // Complete the function
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]==arr[i+1] && arr[i]!=0){
+                arr[i]*=2;
+                arr[i+1]=0;
+                i++;
+            }
+        }
+        int i=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]!=0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int x:arr)ans.add(x);
+        return ans;
+    }
+    
+}
