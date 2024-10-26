0b37616d19a5822a52e85372fe599fd34dde5c53
VedanshPundir <vedanshpundir43@gmail.com>
2024-10-26 19:19:03 +0530

    removed .class

--- October 2024/OccurrenceOfinteger.java
+++ October 2024/OccurrenceOfinteger.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
-
 public class OccurrenceOfinteger {
     
-  /* static class Node {
-        int data;
-        Node next;
-
-        Node(int a) {
-            data = a;
-            next = null;
-        }
-    }*/
   
-
-    public static int count(Node head, int key) {
+  public static int count(Node head, int key) {
         int count = 0;
         Node temp = head;
         
@@ -27,25 +15,5 @@ public class OccurrenceOfinteger {
         return count;
     }
 
-    /*public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter number of nodes:");
-        int n = sc.nextInt();
-        Node head = new Node(sc.nextInt()); 
-        Node tail = head;
-        
-        for (int i = 1; i < n; i++) {
-            tail.next = new Node(sc.nextInt());
-            tail = tail.next;
-        }
-        
-        System.out.println("Enter key value:");
-        int key = sc.nextInt();
-        
-        System.out.println("Output:");
-        System.out.println(count(head, key));
-        
-        sc.close();
-    }*/
+    
 }
