b63f7d3228137f860ecf3198516fb0bf8d74f84b
VedanshPundir <vedanshpundir43@gmail.com>
2024-10-26 17:07:32 +0530

    Updated the POTD of 26 Oct

--- October 2024/OccurrenceOfinteger.java
+++ October 2024/OccurrenceOfinteger.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class OccurrenceOfinteger {
     
-    // Node class to represent each node in the linked list
-    static class Node {
+  /* static class Node {
         int data;
         Node next;
 
@@ -11,14 +10,13 @@ public class OccurrenceOfinteger {
             data = a;
             next = null;
         }
-    }
+    }*/
+  
 
-    // Method to count occurrences of the given key in the linked list
     public static int count(Node head, int key) {
         int count = 0;
         Node temp = head;
         
-        // Traverse the linked list
         while (temp != null) {
             if (temp.data == key) {
                 count++;
@@ -29,29 +27,25 @@ public class OccurrenceOfinteger {
         return count;
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read the number of nodes in the linked list
         System.out.println("Enter number of nodes:");
         int n = sc.nextInt();
-        Node head = new Node(sc.nextInt());  // First node
+        Node head = new Node(sc.nextInt()); 
         Node tail = head;
         
-        // Create the linked list
         for (int i = 1; i < n; i++) {
             tail.next = new Node(sc.nextInt());
             tail = tail.next;
         }
         
-        // Read the key to count
         System.out.println("Enter key value:");
         int key = sc.nextInt();
         
-        // Output the count of occurrences of the key
         System.out.println("Output:");
         System.out.println(count(head, key));
         
         sc.close();
-    }
+    }*/
 }
