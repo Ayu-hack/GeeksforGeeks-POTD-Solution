6b0ad6e26254a9bee249d33a2395d788ecdd9512
Dnyaneshwar Dhananjay Potdar <111481666+Dnyanesh182@users.noreply.github.com>
2024-10-28 14:32:18 +0530

    Update Three_way_partitioning.java

--- April 2024/Three_way_partitioning.java
+++ April 2024/Three_way_partitioning.java
@@ -1,27 +1,23 @@
-class Solution{
+class Solution {
     
-    public void threeWayPartition(int array[], int a, int b)
-    {
+    public void threeWayPartition(int array[], int a, int b) {
         int start = 0, end = array.length - 1;
         
         for (int i = 0; i <= end;) {
-             if (array[i] < a) {
+            if (array[i] < a) {
                 int temp = array[i];
                 array[i] = array[start];
                 array[start] = temp;
                 start++;
                 i++;
-            }
-            
-            else if (array[i] > b) {
+            } else if (array[i] > b) {
                 int temp = array[i];
                 array[i] = array[end];
                 array[end] = temp;
-                end--;
-            }
-             else {
+                end--; // Do not increment i here, as the new element at array[i] needs to be checked.
+            } else {
                 i++;
             }
         }
-}
+    }
 }
