a35ad7868041437c11cd717cc3478713f4d3e25b
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2024-10-29 20:09:40 +0530

    Create Quick Sort on Linked List(29 oct).java

---
+++ October 2024/Quick Sort on Linked List(29 oct).java
@@ -0,0 +1,32 @@
+class GfG {
+    static void partition(Node pivot, Node left[], Node right[]){
+        Node temp = pivot.next;
+        while(temp!=null){
+            Node nxt = temp.next;
+            if(temp.data<=pivot.data){
+                temp.next=left[0];
+                left[0]=temp;
+            }
+            else{
+                temp.next=right[0];
+                right[0]=temp;
+            }
+            temp=nxt;
+        }
+    }
+    public static Node quickSort(Node node) {
+        // Your code here
+        if(node==null || node.next==null)return node;
+        Node left[]=new Node[1];
+        Node right[]=new Node[1];
+        partition(node,left,right);
+        left[0] = quickSort(left[0]);
+        right[0] = quickSort(right[0]);
+        Node traversal = left[0];
+        while(traversal!=null && traversal.next!=null)traversal=traversal.next;
+        if(traversal!=null)traversal.next=node;
+        else left[0]=node;
+        node.next=right[0];
+        return left[0];
+    }
+}
