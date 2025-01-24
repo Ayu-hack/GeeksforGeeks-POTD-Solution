4356a204d12d705f113257370c05043941a4dc2c
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2025-01-24 20:57:04 +0530

    Create Detect Loop in linked list(24 Jan).java

---
+++ January 2025/Detect Loop in linked list(24 Jan).java
@@ -0,0 +1,15 @@
+class Solution {
+    // Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head) {
+        // Add code here
+        Node slow=head, fast=head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
