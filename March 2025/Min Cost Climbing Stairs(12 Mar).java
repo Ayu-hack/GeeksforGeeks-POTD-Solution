152530d0a6cec7f5a2e79676779d322b1555702e
Ayush Tiwari <92630471+Ayu-hack@users.noreply.github.com>
2025-03-12 22:12:35 +0530

    Create Min Cost Climbing Stairs(12 Mar).java

---
+++ March 2025/Min Cost Climbing Stairs(12 Mar).java
@@ -0,0 +1,14 @@
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int first = cost[0];
+        int second = cost[1];
+
+        for (int i = 2; i < n; i++) {
+            int curr = cost[i] + Math.min(first, second);
+            first = second;
+            second = curr;
+        }
+        return Math.min(first, second);
+    }
+}
